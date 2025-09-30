62dfed2b75e784bfd24956787b0d0a311fabe85d
Cardoso <ajca.dev@gmail.com>
2025-09-30 09:21:15 -0400

    feat(port): create user repository port

---
+++ src/domain/users/repositories/user.repository.ts
@@ -0,0 +1,7 @@
+import { User } from '@domain/users/entities/user.entity';
+
+export abstract class UserRepository {
+  abstract findById(id: string): Promise<User | null>;
+  abstract findByEmail(emailRaw: string): Promise<User | null>;
+  abstract save(user: User): Promise<void>;
+}
