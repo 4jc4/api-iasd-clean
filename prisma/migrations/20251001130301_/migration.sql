d89b6999f31406fef6080ec5d5703a5c9b10d747
Cardoso <ajca.dev@gmail.com>
2025-10-01 09:03:22 -0400

    chore(docker-setup): add local PostgreSQL database with docker-compose

---
+++ prisma/migrations/20251001130301_/migration.sql
@@ -0,0 +1,50 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "refresh_tokens" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "secretHash" TEXT NOT NULL,
+    "familyId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "revokedAt" TIMESTAMP(3),
+    "replacedByTokenId" TEXT,
+    "createdByIp" TEXT,
+    "userAgent" TEXT,
+    "lastUsedAt" TIMESTAMP(3),
+
+    CONSTRAINT "refresh_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "refresh_tokens_userId_idx" ON "refresh_tokens"("userId");
+
+-- CreateIndex
+CREATE INDEX "refresh_tokens_familyId_idx" ON "refresh_tokens"("familyId");
+
+-- CreateIndex
+CREATE INDEX "refresh_tokens_userId_familyId_revokedAt_replacedByTokenId__idx" ON "refresh_tokens"("userId", "familyId", "revokedAt", "replacedByTokenId", "createdAt");
+
+-- CreateIndex
+CREATE INDEX "refresh_tokens_userId_createdAt_idx" ON "refresh_tokens"("userId", "createdAt");
+
+-- AddForeignKey
+ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
